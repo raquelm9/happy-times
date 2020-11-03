f76502f4bbf9ff0fb8b0463847abe042e3d96630
Agata Palka <agatapalka@MacBook-Pro.local>
2020-11-03 11:08:13 -0700

    RestaurantDetail page created - Agata

---
+++ client/src/pages/RestaurantDetail.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import OneImage from '../components/OneImage/OneImage';
+import Grid from '../components/Grid/Grid'
+
+
+const RestaurantDetail = () => {
+    return (
+        <>
+        <OneImage />
+        <Grid />
+
+        </>
+    );
+};
+
+export default RestaurantDetail;
