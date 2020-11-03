5c5951fc8e0f51a7d7de2711f54ae1447168bc84
Agata Palka <agatapalka@MacBook-Pro.local>
2020-11-03 11:08:37 -0700

    component created - Agata

---
+++ client/src/components/DetailRestaurantSegment/DetailRestaurantSegment.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const DetailRestaurantSegment = () => {
+    return (
+        <>
+        <h3 className='ui header'>Restaurant Name</h3>
+       <h4 className='address'>Address</h4>
+       <a href='www.google.ca' target='_blank' >Link</a>
+       <p>description</p>
+        </>
+
+
+    );
+};
+
+export default DetailRestaurantSegment;
