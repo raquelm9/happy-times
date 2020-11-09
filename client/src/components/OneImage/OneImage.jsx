460acbc489dfdcdd996e090733e95a14b6824757
Agata Palka <agatapalka@MacBook-Pro.local>
2020-11-09 13:49:22 -0700

    changing to class based component

--- client/src/components/OneImage/OneImage.jsx
+++ client/src/components/OneImage/OneImage.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import '../OneImage/OneImage.css'
 
 
-const OneImage = () => {
+class OneImage extends React.Component {
+    render() {
     return (      
         <img className="ui fluid centered rounded image" id="photo" 
         src="https://images.squarespace-cdn.com/content/v1/54d5402be4b0fa3826c298ce/1428959791721-C2GGA6NJWINHB6UW2HLN/ke17ZwdGBToddI8pDm48kLkXF2pIyv_F2eUT9F60jBl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0iyqMbMesKd95J-X4EagrgU9L3Sa3U8cogeb0tjXbfawd0urKshkc5MgdBeJmALQKw/finals-0001.jpg" 
@@ -10,5 +11,6 @@ const OneImage = () => {
         />
     );
 };
+}
 
 export default OneImage;
