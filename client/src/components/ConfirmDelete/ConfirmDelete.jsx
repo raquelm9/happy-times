2d589b27f9bd672ab08fc7925f6777d2a5fb8ad9
Raquel Mijares <raquelm9@hotmail.com>
2020-11-18 18:56:04 -0700

    delete alert

---
+++ client/src/components/ConfirmDelete/ConfirmDelete.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
+
+export class ConfirmDelete extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Modal show={this.props.isOpen}>
+        <Modal.Header>
+          <Modal.Title>Delete Message</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div class="alert alert-danger" role="alert">
+            Are you sure you want to permanently delete this record?
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={this.props.onCancel}>
+            Close
+          </Button>
+          <Button variant="danger" onClick={this.props.onConfirm}>
+            Confirm
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
--- client/src/components/RestListingComp/RestListingComp.jsx
+++ client/src/components/RestListingComp/RestListingComp.jsx
@@ -3,12 +3,15 @@ import { withRouter } from "react-router-dom";
 import { todaysHappyHour } from "../../helpers/happy_hour";
 import { addressLabel } from "../../helpers/address";
 import { HttpService } from "../../services/http-service";
+import { ConfirmDelete } from "../ConfirmDelete/ConfirmDelete";
 
 import "./RestListingComp.css";
 
 class RestListingComp extends React.Component {
   constructor(props) {
     super(props);
+    this.deleteRest = this.deleteRest.bind(this);
+    this.state = { isOpen: false };
   }
 
   timeHappyHour() {
@@ -63,13 +66,24 @@ class RestListingComp extends React.Component {
       .then(() => this.props.onDelete());
   }
 
+  openModal(event) {
+    //prevents the page to reroute to rest detail
+    event.stopPropagation();
+
+    this.setState({ isOpen: true });
+  }
+
+  closeModal() {
+    this.setState({ isOpen: false });
+  }
+
   showCanDelete() {
     if (this.props.canDelete) {
       return (
         <div className="deleteButton">
           <i
             className="trash alternate icon"
-            onClick={this.deleteRest.bind(this)}
+            onClick={this.openModal.bind(this)}
           ></i>
         </div>
       );
@@ -87,11 +101,21 @@ class RestListingComp extends React.Component {
     const restaurant = this.props.restaurant;
 
     return (
-      <div className="restaurant-card" onClick={this.viewHappyHour.bind(this)}>
-        <p>Restaurant Name: {restaurant.name}</p>
-        <p>Address: {addressLabel(restaurant)}</p>
-        <>{this.showCanDelete()}</>
-      </div>
+      <>
+        <div
+          className="restaurant-card"
+          onClick={this.viewHappyHour.bind(this)}
+        >
+          <p>Restaurant Name: {restaurant.name}</p>
+          <p>Address: {addressLabel(restaurant)}</p>
+          <>{this.showCanDelete()}</>
+        </div>
+        <ConfirmDelete
+          isOpen={this.state.isOpen}
+          onCancel={this.closeModal.bind(this)}
+          onConfirm={this.deleteRest.bind(this)}
+        ></ConfirmDelete>
+      </>
     );
   }
 }
