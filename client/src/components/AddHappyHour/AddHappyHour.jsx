3ac7580fbbc35ceee1da518afe83f4d5a764f00f
Raquel Mijares <raquelm9@hotmail.com>
2020-11-24 20:34:47 -0700

    restaurant id passed to add happy hour

--- client/src/components/AddHappyHour/AddHappyHour.jsx
+++ client/src/components/AddHappyHour/AddHappyHour.jsx
@@ -5,10 +5,18 @@ import { withRouter } from "react-router-dom";
 class AddHappyHour extends React.Component {
   constructor(props) {
     super(props);
+    const queryString = this.props.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const idRestaurant = urlParams.get("id");
+
+    this.idRestaurant = idRestaurant;
   }
 
   addNewHappyHour() {
-    this.props.history.push("/admin/restaurant/happy-hour/information");
+    this.props.history.push({
+      pathname: "/admin/restaurant/happy-hour/information",
+      search: "restaurantId=" + this.idRestaurant,
+    });
   }
 
   render() {
