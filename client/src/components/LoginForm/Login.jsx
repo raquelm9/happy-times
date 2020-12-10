2d14203c5e646d0415322ea882b869ce41c235ec
manuellemendez <manuellemendez@gmail.com>
2020-12-10 10:36:43 -0700

    another change

--- client/src/components/LoginForm/Login.jsx
+++ client/src/components/LoginForm/Login.jsx
@@ -8,82 +8,84 @@ import './Login.css'
 import BeerAnimation from './BeerAnimation'
 
 class Login extends React.Component {
-    async componentDidMount() {
-        try {
-            let res = await fetch('/isLoggedIn', {
-                method: 'post',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                },
-            })
-            let result = await res.json()
-            if (result && result.success) {
-                UserStore.loading = false
-                UserStore.isLoggedIn = true
-                UserStore.username = result.username
-            } else {
-                UserStore.loading = false
-                UserStore.isLoggedIn = false
-            }
-        } catch (e) {
-            UserStore.loading = false
-            UserStore.isLoggedIn = false
-        }
+  async componentDidMount() {
+    try {
+      let res = await fetch('/isLoggedIn', {
+        method: 'post',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      })
+      let result = await res.json()
+      if (result && result.success) {
+        UserStore.loading = false
+        UserStore.isLoggedIn = true
+        UserStore.username = result.username
+      } else {
+        UserStore.loading = false
+        UserStore.isLoggedIn = false
+      }
+    } catch (e) {
+      UserStore.loading = false
+      UserStore.isLoggedIn = false
     }
+  }
 
-    async doLogout() {
-        try {
-            let res = await fetch('/logout', {
-                method: 'post',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                },
-            })
-            let result = await res.json()
-            if (result && result.success) {
-                UserStore.isLoggedIn = false
-                UserStore.username = ''
-            }
-        } catch (e) {
-            console.log(e)
-        }
+  async doLogout() {
+    try {
+      let res = await fetch('/logout', {
+        method: 'post',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      })
+      let result = await res.json()
+      if (result && result.success) {
+        UserStore.isLoggedIn = false
+        UserStore.username = ''
+      }
+    } catch (e) {
+      console.log(e)
     }
+  }
 
-    render() {
-        if (UserStore.loading) {
-            return (
-                <div className="appL">
-                    <div className="containerL">loading, please wait..</div>
-                </div>
-            )
-        } else {
-            if (UserStore.isLoggedIn) {
-                return (
-                    <div className="appL">
-                        <div className="containerL">
-                            Welcome{UserStore.username}
-                            <SubmitButton
-                                text={'Log out'}
-                                disable={false}
-                                onClick={() => this.doLogout()}
-                            />
-                        </div>
-                    </div>
-                )
-            }
+  render() {
+    if (UserStore.loading) {
+      return (
+        <div className="appL">
+          <div className="containerL">loading, please wait..</div>
+        </div>
+      )
+    } else {
+      if (UserStore.isLoggedIn) {
+        return (
+          <div className="appL">
+            <div className="containerL">
+              Welcome{UserStore.username}
+              <SubmitButton
+                text={'Log out'}
+                disable={false}
+                onClick={() => this.doLogout()}
+              />
+            </div>
+          </div>
+        )
+      }
 
-            return (
-                <div className="appL">
-                    <div className="containerL">
-                        <LoginForm />
-                        <BeerAnimation />
-                    </div>
-                </div>
-            )
-        }
+      return (
+        <div className="appL ">
+          <div className="containerL ">
+            <LoginForm />
+            <BeerAnimation />
+          </div>
+
+          <SubmitButton />
+        </div>
+      )
     }
+  }
 }
 
 export default observer(Login)
