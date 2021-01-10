64cd5df7fec9a547a9d22e86f1adf0b229defa53
Raquel Mijares <raquelm9@hotmail.com>
2021-01-09 22:18:14 -0700

    fix map tile layer

--- client/src/pages/Map.jsx
+++ client/src/pages/Map.jsx
@@ -82,8 +82,8 @@ class MapClient extends React.Component {
           scrollWheelZoom={true}
         >
           <TileLayer
-            attribution='&copy; <a href="http://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="http://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-            url="http://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           {this.state.restaurants.map(this.buildMarkerCard)}
         </MapContainer>
